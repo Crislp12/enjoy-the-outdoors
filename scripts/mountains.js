a56755032952d34a9903076746cd6bd7aa9321af
Crislp12 <cristopher.picopinos@gmail.com>
2024-11-06 15:34:43 -0500

    feat: created filtering system for both pages

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -1,7 +1,96 @@
-"use strict";
+const mountainfilter = document.getElementById('mountain-filter');
+const gallleryContainer = document.querySelector('.mountains-gallary');
+const singleGalleryContainer = document.querySelector('.mountain-gallary');
+const modalTitle = document.querySelector('.modal-title');
+const modalBody = document.querySelector('.modal-body');
+const modaltextElvation = document.getElementById('modal-textElevation');
+const modaltextLat = document.getElementById('modal-textLat');
+const modaltextLng = document.getElementById('modal-textLng');
 
-function loadMountains(params) {
-  for (const mountain of mountainsArray) {
-    console.log(mountain);
+document.addEventListener('DOMContentLoaded', () => {
+  populateSelectOptions();
+  createCardElements(mountainsArray);
+});
+
+function populateSelectOptions() {
+  mountainsArray.forEach((mountain) => {
+    const newOption = document.createElement('option');
+    newOption.innerText = mountain.name;
+    mountainfilter.appendChild(newOption);
+  });
+}
+
+mountainfilter.addEventListener('change', () => {
+  const input = mountainfilter.value;
+  
+  console.log(input);
+  if (input === 'All') {
+    singleGalleryContainer.style.display = 'none';
+    gallleryContainer.style.display = 'grid';
+  } else {
+    gallleryContainer.style.display = 'none';
+    singleGalleryContainer.style.display = 'flex';
+    singleGalleryContainer.innerHTML = ""
+    const mountain = mountainsArray.find((mountain)=> mountain.name === input);
+    createCardElement([mountain])
   }
+});
+
+function createCardElements(mountainsArray) {
+  for (const mountain of mountainsArray) {
+    const cardDiv = document.createElement('div');
+    cardDiv.classList.add('mountain-gallary-cards');
+    cardDiv.style.backgroundImage = `url('images/${mountain.img}')`;
+
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.className = 'btn btn-primary';
+    button.setAttribute('data-bs-toggle', 'modal');
+    button.setAttribute('data-bs-target', '#staticBackdrop');
+    button.innerText = mountain.name;
+
+    button.addEventListener('click', () => {
+      modalTitle.innerText = mountain.name;
+      modalBody.innerText = mountain.desc;
+      modaltextElvation.innerText = mountain.elevation;
+      modaltextLat.innerText = mountain.coords.lat;
+      modaltextLng.innerText = mountain.coords.lng;
+    });
+
+    cardDiv.appendChild(button);
+
+    gallleryContainer.appendChild(cardDiv);
+  }
+}
+
+
+function createCardElement(mountainsArray) {
+  for (const mountain of mountainsArray) {
+    const cardDiv = document.createElement('div');
+    cardDiv.classList.add('mountain-gallary-card');
+    cardDiv.style.backgroundImage = `url('images/${mountain.img}')`;
+
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.className = 'btn btn-primary';
+    button.setAttribute('data-bs-toggle', 'modal');
+    button.setAttribute('data-bs-target', '#staticBackdrop');
+    button.innerText = mountain.name;
+
+    button.addEventListener('click', () => {
+      modalTitle.innerText = mountain.name;
+      modalBody.innerText = mountain.desc;
+      modaltextElvation.innerText = mountain.elevation;
+      modaltextLat.innerText = mountain.coords.lat;
+      modaltextLng.innerText = mountain.coords.lng;
+    });
+
+    cardDiv.appendChild(button);
+
+    singleGalleryContainer.appendChild(cardDiv);
+  }
+}
+
+function generateCardElement(input) {
+  const cardDiv = document.createElement('div');
 }
--- scripts/national-parks.js
+++ scripts/national-parks.js
@@ -1,6 +1,101 @@
+const table = document.getElementById('mountain-table');
+const stateRadio = document.getElementById('state');
+const typeRadio = document.getElementById('type');
+const categorySelect = document.getElementById('categorySelect');
 
-function loadParks() {
-  for (const park of nationalParksArray) {
+document.addEventListener('DOMContentLoaded', () => {
+  renderDataOnTable(nationalParksArray);
+});
+
+stateRadio.addEventListener('click', () => {
+  categorySelect.length = 1;
+
+  if (stateRadio.checked) {
+    locationsArray.forEach((location) => {
+      const newOption = document.createElement('option');
+      newOption.innerText = location;
+      categorySelect.appendChild(newOption);
+    });
+  }
+});
+
+typeRadio.addEventListener('click', () => {
+  categorySelect.length = 1;
+
+  if (typeRadio.checked) {
+    parkTypesArray.forEach((type) => {
+      const newOption = document.createElement('option');
+      newOption.innerText = type;
+      categorySelect.appendChild(newOption);
+    });
+  }
+});
+
+categorySelect.addEventListener('change', () => {
+  const input = categorySelect.value;
+  removeAllChildren(table);
+
+  if (input === 'All') {
+    renderDataOnTable(nationalParksArray);
+  } else if (stateRadio.checked) {
+    const filterMountains = nationalParksArray.filter((park) => park.State === input);
+    renderDataOnTable(filterMountains);
+  } else if (typeRadio.checked) {
+    const filterMountains = nationalParksArray.filter((park) => park.LocationName.includes(input));
+    renderDataOnTable(filterMountains);
+  }
+  
+});
+
+// helper fucntion to remove all child elements from the table
+function removeAllChildren(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
   }
 }
 
+function renderDataOnTable(nationalParksArray) {
+  for (const mountain of nationalParksArray) {
+    const row = document.createElement('tr');
+
+    const tableDataName = document.createElement('td');
+    tableDataName.innerText = mountain.LocationName || ''; // Avoid errors if data is missing
+    row.appendChild(tableDataName);
+
+    const tableDataAddress = document.createElement('td');
+    tableDataAddress.innerText = mountain.Address || '';
+    row.appendChild(tableDataAddress);
+
+    const tableDataCity = document.createElement('td');
+    tableDataCity.innerText = mountain.City || '';
+    row.appendChild(tableDataCity);
+
+    const tableDataState = document.createElement('td');
+    tableDataState.innerText = mountain.State || '';
+    row.appendChild(tableDataState);
+
+    const tableDataZipCode = document.createElement('td');
+    tableDataZipCode.innerText = mountain.ZipCode || '';
+    row.appendChild(tableDataZipCode);
+
+    const tableDataPhone = document.createElement('td');
+    tableDataPhone.innerText = mountain.Phone || '';
+    row.appendChild(tableDataPhone);
+
+    const tableDataNameWebsite = document.createElement('td');
+
+    if (mountain.Visit) {
+      const link = document.createElement('a');
+      link.href = mountain.Visit;
+      link.innerText = 'Visit';
+      link.target = '_blank';
+      tableDataNameWebsite.appendChild(link);
+    } else {
+      tableDataNameWebsite.innerText = 'N/A';
+    }
+
+    row.appendChild(tableDataNameWebsite);
+
+    table.appendChild(row);
+  }
+}
